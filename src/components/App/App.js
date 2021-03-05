d445d24b76bfd253b67bad44bd9d635e84d9a5d9
Loreen-netizen <loreenmuusha@gmail.com>
2021-03-05 14:14:14 +0200

    app header set up

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Header from "../Header/Header"
+
+
+// const GetTrains = async () => {
+
+//   let trainsObj2 = await getData('/data/trains.json');
+//   let arr = [];
+//   let obj2 = arr.push(trainsObj2) 
+//   console.log({obj2})
+//   await obj2.map(train => { return <li>{train}</li> });
+
+// }
+const App = () => {
+
+  const getData = async () => {
+    const stationsResponse = await fetch('/data/stations.json');
+    const trainsRespnse = await fetch('/data/trains.json');
+
+    const stationsData = await stationsResponse.json();
+    const trainsData = await trainsRespnse.json();
+    console.log (stationsData);
+    console.log (trainsData);
+
+  }
+
+  return (
+    <div className="App">
+      <Header/>
+      {/* <ul>
+        <GetTrains props={trainsObject}></GetTrains>
+      </ul> */}
+{/* <button onClick = {getData}>logData</button> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = ()=>{
+    return(
+        
+        <header className="header">Train Schedule
+        <h5>no need to be late, always use trains app!</h5></header>
+    )
+}
+
+export default Header;
